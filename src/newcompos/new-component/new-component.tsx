9977894ece619076eb7f63f5b65c9d6b257204c6
Rodrigo Pinto <workstationpt@hotmail.com>
2023-09-02 17:34:36 +0100

    +

--- src/newcompos/new-component/new-component.tsx
+++ src/newcompos/new-component/new-component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { IconPicker } from '@pnp/spfx-controls-react';
 
 export interface NewComponentProps {
     className?: string;
 }
 
 export const NewComponent: React.FC<NewComponentProps> = ({ className = '' }) => (
-    <div className={className}>NewComponent</div>
+    <div className={className}>
+        <IconPicker />NewComponent</div>
 );
